601fcda4d0449ccef1199680b0b0066cdb4ff7f4
2Peti <44583946+2Peti@users.noreply.github.com>
2020-11-23 12:50:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,6 @@ bash ubuntu.sh
 
 sed -i 's/#command+=" -b \/data/command+=" -b \/data/g' start-ubuntu.sh
 
-#termux-setup-storage
-#mkdir ~/storage/shared/Geyser/
-#ln -s ~/storage/shared/Geyser/ ~/Geyser
 
 mkdir ~/Geyser/
 cd ~/Geyser/
@@ -24,9 +21,8 @@ cat >.profile <<EOM
 apt update
 apt install -y default-jre
 
-echo
+echo Made by Cochces™
 echo Geyser has been installed!
-#echo You can access the config file using your phones file manager, there should be a folder called 'Geyser'.
 echo You can now start Geyser at any time using
 echo ./ubuntu_directory/start-ubuntu.sh
 echo cd Geyser
